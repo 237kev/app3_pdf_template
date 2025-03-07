a323042c7b5c92dd324615ab2aae67830b0d658a
Kevin <k3v1n.k4md3m@gmail.com>
2025-03-07 22:23:49 +0100

    pages added

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello')
+from fpdf import FPDF
+import pandas as pd
+
+df = pd.read_csv('topics.csv')
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.add_page()
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family='Times',style='B',size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=f"{row['Topic']}",align='L', ln=1, border=1)
+    pdf.line(10,22,200,22)
+
+    range()
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
+
+pdf.output('output.pdf')
